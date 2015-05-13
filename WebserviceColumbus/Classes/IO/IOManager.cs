522e5006950ee8388e95a847d4f5f07d815dcead
RoyBekkering <roy.bekkering@student.stenden.com>
2015-05-13 15:28:44 +0200

    .

---
+++ WebserviceColumbus/Classes/IO/IOManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace WebserviceColumbus.Classes.IO
+{
+    public static class IOManager
+    {
+        public static string ReadFile(string filePath)
+        {
+            if(File.Exists(filePath)) {
+                string[] lines = File.ReadAllLines(filePath);
+                string result = string.Empty;
+                foreach (string line in lines) {
+                    result = result + line;
+                }
+                return result;
+            }
+            return null;
+        }
+    }
+}
