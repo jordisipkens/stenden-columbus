cced5adceaed5980546eeb0cb5383412e7869736
Jordi Sipkens <jordi@gillz.nl>
2015-05-19 18:00:51 +0200

    ADDED - Menu fragments with layoutfiles

---
+++ Columbus/app/src/main/java/columbus/stenden/nl/columbus/Fragments/AccountFragment.java
@@ -0,0 +1,54 @@
+package columbus.stenden.nl.columbus.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import columbus.stenden.nl.columbus.R;
+
+/**
+ * Created by Jordi on 19/05/15.
+ */
+public class AccountFragment extends Fragment{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_account, container, false);
+
+        return v;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
---
+++ Columbus/app/src/main/java/columbus/stenden/nl/columbus/Fragments/HomeFragment.java
@@ -0,0 +1,61 @@
+package columbus.stenden.nl.columbus.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import columbus.stenden.nl.columbus.R;
+
+/**
+ * Created by Jordi on 19/05/15.
+ *
+ * Fragment for the home screen of the user.
+ * Should display travels when they are bounded to the current account.
+ */
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+        super();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_main, container, false);
+
+        return v;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
