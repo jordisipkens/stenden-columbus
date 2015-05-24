af66af1f86d494160b037d477f25d2c01ef9564d
Marc Tibben <marctibben@hotmail.com>
2015-05-24 19:30:23 +0200

    ADDED - Added section to layout for dynamic scripts and styles per view. Added Google Maps on view travel page (not done yet, only markers have to been added).

--- ColombusWebapplicatie/Models/LocationDetails.cs
+++ ColombusWebapplicatie/Models/LocationDetails.cs
@@ -9,7 +9,7 @@ namespace ColombusWebapplicatie.Models
     {
         public int ID { get; set; }
         public string Name { get; set; }
-        public string Adres { get; set; }
+        public string Address { get; set; }
         public string PhoneNumber { get; set; }
         public string PlaceID { get; set; }
         public Coordinates Coordinates { get; set; }
