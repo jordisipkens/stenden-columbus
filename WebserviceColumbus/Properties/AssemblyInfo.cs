df986a3a0d9571f65df236ff2381350bc7ba722e
RoyBekkering <roy.bekkering@student.stenden.com>
2015-05-26 01:58:29 +0200

    CHANGED - Build version increment

--- WebserviceColumbus/Properties/AssemblyInfo.cs
+++ WebserviceColumbus/Properties/AssemblyInfo.cs
@@ -3,15 +3,14 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following set of attributes.
-// Change these attribute values to modify the information associated with an assembly.
 [assembly: AssemblyTitle("WebserviceColumbus")]
 [assembly: AssemblyDescription("Webservice of the Columbus Project")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Stenden made by Students")]
 [assembly: AssemblyProduct("Webservice Columbus")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTrademark("Stenden-Columbus")]
+[assembly: AssemblyCulture("en-US")]
 
 // Setting ComVisible to false makes the types in this assembly not visible to COM components. If
 // you need to access a type in this assembly from COM, set the ComVisible attribute to true on that type.
@@ -29,5 +28,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
