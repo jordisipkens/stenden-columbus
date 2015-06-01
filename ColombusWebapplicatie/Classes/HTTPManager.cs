c9932ec18f650701984d2a71cd0553b8f996bd10
RoyBekkering <roy.bekkering@student.stenden.com>
2015-06-01 11:05:15 +0200

    ADDED - HttpManager

---
+++ ColombusWebapplicatie/Classes/HTTPManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ColombusWebapplicatie.Classes
+{
+    public static class HTTPManager
+    {
+        public static T Request<T>(string url)
+        {
+            return default(T);
+        }
+    }
+}
