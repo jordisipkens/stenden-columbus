83717d66dd8d601be996840f8aca8229dddd5bab
Jordi Sipkens <jordisipkens@gmail.com>
2015-06-02 23:03:05 +0200

    ADDED - New JSON Object Photo

---
+++ Columbus_2/app/src/main/java/stenden/nl/columbus/GSON/Objects/Photo.java
@@ -0,0 +1,59 @@
+package stenden.nl.columbus.GSON.Objects;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Jordi on 6/2/2015.
+ */
+public class Photo {
+    @SerializedName("ID")
+    public int id;
+    @SerializedName("Caption")
+    public String caption;
+    @SerializedName("URL")
+    public String url;
+    @SerializedName("LocationId")
+    public int locationId;
+    @SerializedName("TravelId")
+    public int travelId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(int locationId) {
+        this.locationId = locationId;
+    }
+
+    public int getTravelId() {
+        return travelId;
+    }
+
+    public void setTravelId(int travelId) {
+        this.travelId = travelId;
+    }
+}
