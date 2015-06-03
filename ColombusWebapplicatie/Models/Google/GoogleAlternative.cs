ff682d11764c4af4c2cfcd8faec1a2f99e112add
RoyBekkering <roy.bekkering@student.stenden.com>
2015-06-03 09:44:23 +0200

    ADDED - Missing models

---
+++ ColombusWebapplicatie/Models/Google/GoogleAlternative.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace ColombusWebapplicatie.Models.Google
+{
+    public class GoogleAlternative
+    {
+        [JsonProperty("PlaceID")]
+        public string place_id { get; set; }
+
+        [JsonProperty("Scope")]
+        public string scope { get; set; }
+    }
+}
---
+++ ColombusWebapplicatie/Models/Photo.cs
@@ -0,0 +1,15 @@
+﻿namespace ColombusWebapplicatie.Models
+{
+    public class Photo
+    {
+        public int ID { get; set; }
+
+        public string Caption { get; set; }
+
+        public string URL { get; set; }
+
+        public int LocationID { get; set; }
+
+        public int TravelID { get; set; }
+    }
+}
