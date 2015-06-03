01b10cbd736fc37c99a048b338986b0a83615963
RoyBekkering <roy.bekkering@student.stenden.com>
2015-06-03 11:34:58 +0200

    CHANGED - TravelController

--- ColombusWebapplicatie/Controllers/TravelController.cs
+++ ColombusWebapplicatie/Controllers/TravelController.cs
@@ -1,4 +1,5 @@
-﻿using ColombusWebapplicatie.Models.Google;
+﻿using ColombusWebapplicatie.Classes.Http;
+using ColombusWebapplicatie.Models.Google;
 using ColombusWebapplicatie.Models.Travel;
 using Newtonsoft.Json;
 using System.Collections.Generic;
@@ -22,6 +23,30 @@ namespace ColombusWebapplicatie.Controllers
             List<Travel> travels = JsonConvert.DeserializeObject<List<Travel>>(streamReader.ReadToEnd());
             // Return the regarding view.
             return View(travels);
+        }
+
+        public ActionResult View(int id = 0)
+        {
+            if(id != 0) {
+                List<Travel> travels = JsonSerialization.DeserializeFromFile<List<Travel>>(Server.MapPath("~/Content/json/Travel.json"));
+                /*Travel travel = HTTPManager.WebserviceGetRequest<Travel>("travel/" + id, Request);
+                if(travel != null) {*/
+                return View(travels[id]);
+                //}
+            }
+            return ErrorToIndex("Deze reis bestaat niet (meer)");
+        }
+
+        private GoogleResponse RequestGooglePlaces(string query)
+        {
+            string googleApiKey = "AIzaSyDpXa5VtOKNRA8obETZwkV7vbHzjio-17k";
+            string googleUrl = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + query + "&key=" + googleApiKey;
+            WebRequest request = WebRequest.Create(googleUrl);
+            WebResponse response = request.GetResponse();
+            StreamReader streamReader = new StreamReader(response.GetResponseStream());
+            string responseString = streamReader.ReadToEnd();
+            GoogleResponse googlePlacesResponse = (GoogleResponse)JsonConvert.DeserializeObject<GoogleResponse>(responseString);
+            return googlePlacesResponse;
         }
 
         [HttpGet] // Show add travel page
@@ -59,19 +84,6 @@ namespace ColombusWebapplicatie.Controllers
                 return ErrorToIndex("Deze reis bestaat niet (meer).");
             }
         }
-
-        private GoogleResponse RequestGooglePlaces(string query)
-        {
-            string googleApiKey = "AIzaSyDpXa5VtOKNRA8obETZwkV7vbHzjio-17k";
-            string googleUrl = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + query + "&key=" + googleApiKey;
-            WebRequest request = WebRequest.Create(googleUrl);
-            WebResponse response = request.GetResponse();
-            StreamReader streamReader = new StreamReader(response.GetResponseStream());
-            string responseString = streamReader.ReadToEnd();
-            GoogleResponse googlePlacesResponse = (GoogleResponse)JsonConvert.DeserializeObject<GoogleResponse>(responseString);
-            return googlePlacesResponse;
-        }
-
         [HttpPost] // Search for a location via Google Places API
         public ActionResult SearchLocation(string query)
         {
@@ -79,25 +91,6 @@ namespace ColombusWebapplicatie.Controllers
             return View(response);
         }
 
-        public ActionResult ViewTravel(int? id)
-        {
-            // !!! Important: Not finished, just for testing !!! Supposed to check if the travel
-            // with the regarding id exists.
-            if(id == null) {
-                // Return to the index of the controller with an error message.
-                return ErrorToIndex("Deze reis bestaat niet (meer).");
-            }
-            else {
-                // Load Json file.
-                StreamReader streamReader = new StreamReader(Server.MapPath("~/Content/json/Travel.json"));
-                // Deserialize Json to list of Travel objects.
-                List<Travel> travels = JsonConvert.DeserializeObject<List<Travel>>(streamReader.ReadToEnd());
-                // !!! Important: Not finished, just for testing !!! Supposed to get the travel with
-                // the regarding id.
-                Travel travel = travels.First();
-                // Return the regarding view.
-                return View(travel);
-            }
-        }
+        
     }
 }
