10d471b9ae88f9fa4db0db817bf07a6246646a82
JarnoB <jarnobotke@gmail.com>
2015-06-03 14:47:34 +0200

    CHANGED  -   Travelogue JSON (Title added) CHANGED  -  Traveloguecontroller CHANGED  - Layout (Reisverslagen is now always visible) CHANGED   - some views

--- ColombusWebapplicatie/Controllers/TravelogueController.cs
+++ ColombusWebapplicatie/Controllers/TravelogueController.cs
@@ -9,7 +9,7 @@ namespace ColombusWebapplicatie.Controllers
 {
     public class TravelogueController : BaseController
     {
-
+        private static int maxTitleLenght = 30;
         public ActionResult Index()
         {
             List<Travelogue> model = new List<Travelogue>();
@@ -17,18 +17,15 @@ namespace ColombusWebapplicatie.Controllers
             StreamReader streamReader = new StreamReader(Server.MapPath("~/Content/json/Travelogue.json"));
             // Deserialize Json to list of Travel objects.
             model.Add(JsonConvert.DeserializeObject<Travelogue>(streamReader.ReadToEnd()));
+            foreach(Travelogue travelogue in model)
+            {
+                if(travelogue.Title.Length >= maxTitleLenght)
+                {
+                    travelogue.Title = travelogue.Title.Substring(0, maxTitleLenght-3) + "...";
+                }
+            }
 
             return View("Index", model);
-        }
-
-        //Method for viewing 1 travelogue
-        public ActionResult ViewTravelogue(int? id)
-        {
-            Travelogue model = HTTPManager.WebserviceGetRequest<Travelogue>("Travelogue/" + id, Request);
-            foreach(Paragraph par in model.Paragraphs) {
-                par.AlignImageLeft = (par.ID % 2 == 0);  // Check if the ID is an odd number
-            }
-            return View(model);
         }
 
         public ActionResult CreateTravelogue()
@@ -39,18 +36,11 @@ namespace ColombusWebapplicatie.Controllers
             return View(model);
         }
 
-        private ActionResult AddParagraph(Travelogue model)
-        {
-            if(model.Paragraphs != null) {
-                model.Paragraphs.Add(new Paragraph());
-            }
-            return View("CreateTravelogue", model);
-        }
-
         [AcceptVerbs(HttpVerbs.Post)]
         public ActionResult SubmitButton(Travelogue model)
         {
-            if(Request.Form["AddParagraph"] != null) {
+            if (Request.Form["AddParagraph"] != null)
+            {
                 return AddParagraph(model);
             }
             else if (Request.Form["Publish"] != null)
@@ -61,11 +51,38 @@ namespace ColombusWebapplicatie.Controllers
             else if (Request.Form["Save"] != null)
             {
                 SaveTravelogue(model);
-            return Index();
-        }
+                return Index();
+            }
             return Index();
         }
 
+        //Method for viewing 1 travelogue
+        public ActionResult ViewTravelogue(int? id)
+        {
+            Travelogue model = new Travelogue();
+            // Load Json file.
+            StreamReader streamReader = new StreamReader(Server.MapPath("~/Content/json/Travelogue.json"));
+            // Deserialize Json to list of Travel objects.
+            model = JsonConvert.DeserializeObject<Travelogue>(streamReader.ReadToEnd());
+            //Travelogue model = HTTPManager.WebserviceGetRequest<Travelogue>("Travelogue/" + id, Request);
+            if (model.Title.Length >= maxTitleLenght + 20)
+            {
+                model.Title = model.Title.Substring(0, maxTitleLenght + 17) + "...";
+            }
+            foreach (Paragraph par in model.Paragraphs)
+            {
+                par.AlignImageLeft = (par.ID % 2 == 0);  // Check if the ID is an odd number
+            }
+            return View(model);
+        }
+        private ActionResult AddParagraph(Travelogue model)
+        {
+            if (model.Paragraphs != null)
+            {
+                model.Paragraphs.Add(new Paragraph());
+            }
+            return View("CreateTravelogue", model);
+        }
         private void PublishTravelogue(Travelogue model)
         {
             if (model.ID != 0)
@@ -86,10 +103,5 @@ namespace ColombusWebapplicatie.Controllers
         {
             return HTTPManager.WebservicePostRequest<Travelogue>("Travelogue", Request, model);
         }
-
-
-
-
-
     }
 }
