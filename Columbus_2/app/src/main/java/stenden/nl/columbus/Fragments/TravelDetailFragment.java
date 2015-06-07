a7d1d0f61aa62000eb44ab94a8f2dc1c2fc31791
Jordi Sipkens <jordi@gillz.nl>
2015-06-07 21:09:07 +0200

    ADDED - GSON
    GSON request within creating a correct listview. Listview onItemClick
    also changed to pass a serialised travel object of the selected travel.

--- Columbus_2/app/src/main/java/stenden/nl/columbus/Fragments/TravelDetailFragment.java
+++ Columbus_2/app/src/main/java/stenden/nl/columbus/Fragments/TravelDetailFragment.java
@@ -6,13 +6,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import com.google.gson.Gson;
+
+import stenden.nl.columbus.GSON.Objects.Location;
+import stenden.nl.columbus.GSON.Objects.Travel;
 import stenden.nl.columbus.R;
 
 /**
  * Created by Jordi on 26/05/15.
  */
 public class TravelDetailFragment extends Fragment {
+
+    private Travel mTravel;
+
+    private ListView LocationsList;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,6 +32,15 @@ public class TravelDetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_travel_details, container, false);
+
+        Bundle bundle = getArguments();
+        mTravel = new Gson().fromJson(bundle.getString("travel"), Travel.class);
+
+        if(mTravel != null){
+            // Fill list of locations.
+            Location[] loc = mTravel.getLocations();
+
+        }
 
         return v;
     }
