3fd40b6274cb0f09132388724ce3c33d10a01fea
Jordi Sipkens <jordi@gillz.nl>
2015-06-08 15:43:41 +0200

    CHANGED - Details
    Show all details from the selected Travel. Show them in a list with a
    note button at the end.

--- Columbus_2/app/src/main/java/stenden/nl/columbus/Fragments/TravelDetailFragment.java
+++ Columbus_2/app/src/main/java/stenden/nl/columbus/Fragments/TravelDetailFragment.java
@@ -1,12 +1,16 @@
 package stenden.nl.columbus.Fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 
@@ -21,7 +25,10 @@ public class TravelDetailFragment extends Fragment {
 
     private Travel mTravel;
 
-    private ListView LocationsList;
+    private ListView locationList;
+    private TextView travelTitle, travelDate;
+
+    private LocationAdapter adapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -33,13 +40,24 @@ public class TravelDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_travel_details, container, false);
 
+        locationList = (ListView) v.findViewById(R.id.travel_locations);
+        travelTitle = (TextView) v.findViewById(R.id.travel_title);
+        travelDate = (TextView) v.findViewById(R.id.travel_date);
+
         Bundle bundle = getArguments();
         mTravel = new Gson().fromJson(bundle.getString("travel"), Travel.class);
 
         if(mTravel != null){
+            // Set title and date
+            travelTitle.setText(mTravel.getTitle());
+            travelDate.setText(mTravel.getStartDate() + " / " + mTravel.getEndDate());
+
             // Fill list of locations.
             Location[] loc = mTravel.getLocations();
 
+            adapter = new LocationAdapter(loc, getActivity());
+
+            locationList.setAdapter(adapter);
         }
 
         return v;
@@ -68,5 +86,60 @@ public class TravelDetailFragment extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
+    }
+
+    private class LocationAdapter extends BaseAdapter {
+        private Location[] list;
+        private Context ctx;
+        private View v;
+        private TextView date, title, address, phoneNumber;
+        private Button note;
+
+        public LocationAdapter(Location[] list, Context ctx) {
+            this.list = list;
+            this.ctx = ctx;
+        }
+
+        @Override
+        public int getCount() {
+            return list.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return list[i];
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int position, View view, ViewGroup parent) {
+            v = LayoutInflater.from(ctx).inflate(R.layout.location_list_item,
+                    parent, false);
+            date = (TextView) v.findViewById(R.id.location_date);
+            title = (TextView) v.findViewById(R.id.location_name);
+            address = (TextView) v.findViewById(R.id.location_address);
+            phoneNumber = (TextView) v.findViewById(R.id.location_phonenumber);
+            note = (Button) v.findViewById(R.id.location_note);
+
+            date.setText(date.getText() + list[position].getDate());
+            title.setText(list[position].getLocationDetails().getName());
+            address.setText(address.getText() + list[position].getLocationDetails().getAddress());
+            phoneNumber.setText(phoneNumber.getText() + list[position].getLocationDetails().getPhone());
+
+            final Location loc = list[position];
+
+            note.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                }
+            });
+
+            return v;
+        }
     }
 }
