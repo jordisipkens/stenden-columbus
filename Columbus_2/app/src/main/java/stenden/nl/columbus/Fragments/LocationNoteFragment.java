61724969a75f7a06727b50e992e66ab5f65f55c1
Jordi Sipkens <jordi@gillz.nl>
2015-06-08 15:41:38 +0200

    ADDED - Note fragment & layout voor locations

---
+++ Columbus_2/app/src/main/java/stenden/nl/columbus/Fragments/LocationNoteFragment.java
@@ -0,0 +1,55 @@
+package stenden.nl.columbus.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import stenden.nl.columbus.R;
+
+/**
+ * Created by Jordi on 08/06/15.
+ */
+public class LocationNoteFragment extends Fragment{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_note, container, false);
+
+        
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
