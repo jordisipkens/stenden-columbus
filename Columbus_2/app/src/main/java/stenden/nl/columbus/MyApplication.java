87eebb44ae9e658254541f25b0e6581eaeaa3681
Jordi Sipkens <jordisipkens@gmail.com>
2015-06-10 14:09:07 +0200

    CHANGED - Removed unused methods

--- Columbus_2/app/src/main/java/stenden/nl/columbus/MyApplication.java
+++ Columbus_2/app/src/main/java/stenden/nl/columbus/MyApplication.java
@@ -6,17 +6,4 @@ import android.app.Application;
  * Created by Jordi on 6/9/2015.
  */
 public class MyApplication extends Application{
-    public static boolean isActivityVisible() {
-        return activityVisible;
-    }
-
-    public static void activityResumed() {
-        activityVisible = true;
-    }
-
-    public static void activityPaused() {
-        activityVisible = false;
-    }
-
-    private static boolean activityVisible;
 }
