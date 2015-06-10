c32b6be9c664b392aeaae435a916998618d5dc2e
Jordi Sipkens <jordisipkens@gmail.com>
2015-06-10 14:09:47 +0200

    ADDED - Travelogue screen

---
+++ Columbus_2/app/src/main/java/stenden/nl/columbus/Fragments/TravelogueFragment.java
@@ -0,0 +1,58 @@
+package stenden.nl.columbus.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import stenden.nl.columbus.R;
+
+/**
+ * Created by Jordi on 6/10/2015.
+ */
+public class TravelogueFragment extends Fragment {
+
+    public TravelogueFragment() {
+        super();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_travelogue, container, false);
+
+        return v;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
