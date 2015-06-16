72a638e7119dc3260a52456e1a7c12748be6ffcf
RoyBekkering <roy.bekkering@student.stenden.com>
2015-06-17 01:45:35 +0200

    Revert "ADDED - Travel debug"
    This reverts commit 3f2cfd6245e9a36027850d594ce5ef837eb7f698.

--- WebserviceColumbus/Controllers/TravelController.cs
+++ WebserviceColumbus/Controllers/TravelController.cs
@@ -35,7 +35,7 @@ namespace WebserviceColumbus.Controllers
         public HttpResponseMessage Update([FromBody]Travel travel)
         {
             if(travel != null) {
-                return Request.CreateResponse(HttpStatusCode.OK, new TravelDbManager().TempUpdateOrInsertEntity(travel));
+                return Request.CreateResponse(HttpStatusCode.OK, new TravelDbManager().UpdateOrInsertEntity(travel));
             }
             return Request.CreateResponse(HttpStatusCode.ExpectationFailed);
         }
